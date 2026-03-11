7c7eb2b8255feab71597bd60463ed1a05a32770e
S.Mihai <calipsocalipso91@yahoo.com>
2026-03-11 16:37:55 +0200

    Final fix: Added quotes to pom.xml and updated Reader path

--- src/main/java/utils/Reader.java
+++ src/main/java/utils/Reader.java
@@ -4,36 +4,29 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Reader {
     public static JSONObject json(String name) {
-        String jsonPath = System.getProperty("user.dir") + "\\src\\main\\java\\testData\\files\\" + name + ".json";
+        // Use File.separator to handle paths correctly on both Windows (\) and Linux (/)
+        String jsonPath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
+                + File.separator + "java" + File.separator + "testData" + File.separator + "files"
+                + File.separator + name + ".json";
 
-        //read the json file from the specified location
-        FileReader reader = null;
-        try {
-            reader = new FileReader(jsonPath);
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        //define the parser for the json content
-        JSONParser jsonParser = new JSONParser();
-        //define the JSONObject
         JSONObject obj = null;
-        try {
+
+
+        try (FileReader reader = new FileReader(jsonPath)) {
+            JSONParser jsonParser = new JSONParser();
             obj = (JSONObject) jsonParser.parse(reader);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
+        } catch (IOException | ParseException e) {
+            System.err.println("Eroare la citirea fișierului JSON: " + jsonPath);
             e.printStackTrace();
         }
 
         return obj;
     }
 }
+
